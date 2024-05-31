08ec39c0ca55df0cd249bad98d957e336e6e2b4d
Prince Bhatt <princebhatt316@gmail.com>
2024-05-31 16:39:31 +0530

    Loader Added

--- js/script.js
+++ js/script.js
@@ -8,3 +8,19 @@ a.addEventListener('click', function () {
 c.addEventListener('click', function () {
   b.style.display = 'none';
 });
+
+// Loader...
+
+window.addEventListener('load', function () {
+  var preloader = document.getElementById('preloader-active');
+  setTimeout(function () {
+    preloader.style.transition = 'opacity 0.6s';
+    preloader.style.opacity = '0';
+    preloader.addEventListener('transitionend', function () {
+      preloader.style.display = 'none';
+    });
+  }, 450);
+  setTimeout(function () {
+    document.body.style.overflow = 'visible';
+  }, 450);
+});
